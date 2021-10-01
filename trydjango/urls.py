e4d1140904290e92a654c15ed2777480af6f74ee
JonathaCnB <jotacarlosftb@gmail.com>
2021-09-30 22:15:47 -0400

    arquivos iniciais necessarios apra implatação do app

---
+++ trydjango/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import include, path
+from users.views import login_view, logout_view, register_view
+
+urlpatterns = [
+    path("", include("articles.urls")),
+    path("login/", login_view),
+    path("logout/", logout_view),
+    path("register/", register_view),
+    path("admin/", admin.site.urls),
+]
