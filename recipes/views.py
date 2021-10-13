0afa25edb80891192b4c0db8f049db5b9d34d9cf
JonathaCnB <jotacarlosftb@gmail.com>
2021-10-12 21:37:56 -0400

    add upload ima via htmx com progress bar

--- recipes/views.py
+++ recipes/views.py
@@ -168,6 +168,9 @@ def recipe_ingredient_update_hx_view(request, parent_id=None, id=None):
 
 
 def recipe_ingredient_image_upload_view(request, parent_id=None):
+    template_name = "recipes/upload-image.html"
+    if request.htmx:
+        template_name = "recipes/partials/image-upload-form.html"
     try:
         parent_obj = Recipe.objects.get(id=parent_id, user=request.user)
     except Exception as e:
@@ -182,4 +185,4 @@ def recipe_ingredient_image_upload_view(request, parent_id=None):
         obj = form.save(commit=False)
         obj.recipe = parent_obj
         obj.save()
-    return render(request, "image-form.html", {"form": form})
+    return render(request, template_name, {"form": form})
